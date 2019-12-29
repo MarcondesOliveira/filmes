40dc1d1e4292e1a89e7d6562b86f29e66f47a141
MarcondesOliveira <marcondescurso@outlook.com>
2019-12-29 16:11:45 -0200

    First commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                Pagina home
+            </div>
+        )
+    }
+}
+
+export default Home
